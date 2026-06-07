44a443a33fe7075896699f16aaa2eac01c86047c
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-06-06 23:42:43 -0700

    initial transparency testing (incomplete)

--- engine/src/window.cpp
+++ engine/src/window.cpp
@@ -28,6 +28,9 @@ Window::Window(int width, int height, const char *title)
     glEnable(GL_DEPTH_TEST);
     glEnable(GL_CULL_FACE);
 
+    glEnable(GL_BLEND);
+    glBlendFunc(GL_SRC_ALPHA, GL_ONE_MINUS_SRC_ALPHA);
+
     glViewport(0, 0, width, height);
 }
 
