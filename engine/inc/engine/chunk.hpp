1392c6cfe6951c94703b42f263e5b537e2c97d80
PixelTomato <73261818+PixelTomato@users.noreply.github.com>
2026-06-19 04:14:47 -0700

    refactored chunks for world system rewrite

--- engine/inc/engine/chunk.hpp
+++ engine/inc/engine/chunk.hpp
@@ -5,58 +5,75 @@
 #include <engine/mesh.hpp>
 
 #include <memory>
-#include <atomic>
+#include <mutex>
 #include <array>
 
-class World;
-
-enum class ChunkState : char
+template <typename T> class LockQueue
 {
-    AwaitingData,
-    Generation,
-    AwaitingMesh,
-    Meshing,
-    Ready,
+private:
+    std::mutex mutex;
+    std::vector<T> data;
+
+public:
+    void push(T value)
+    {
+        std::lock_guard lock(mutex);
+
+        data.push_back(std::move(value));
+    }
+
+    std::vector<T> take()
+    {
+        std::lock_guard lock(mutex);
+
+        std::vector<T> out;
+        out.swap(data);
+
+        return out;
+    }
 };
 
-struct ChunkPosition
+struct ChunkKey
 {
     int x;
     int y;
     int z;
 
-    bool operator==(const ChunkPosition &other) const = default;
-
-    glm::vec3 toVec3() const { return glm::vec3(x, y, z); }
+    bool operator==(const ChunkKey &other) const = default;
 };
 
-struct ChunkUpload
+struct DataChunk
 {
-    ChunkPosition position;
+    ChunkKey key;
+
+    std::array<char, 4096> blocks;
+};
+
+struct MeshChunk
+{
+    ChunkKey key;
 
     std::vector<float> vertices;
-
     std::vector<unsigned int> indices;
+};
 
-    ChunkUpload(ChunkPosition position) : position(position) {}
+struct RenderChunk
+{
+    ChunkKey key;
+
+    Mesh mesh;
+};
+
+struct ChunkPack
+{
+    std::shared_ptr<DataChunk> main;
+
+    std::shared_ptr<DataChunk> neighbors[6];
 };
 
 class Chunk
 {
-private:
-    std::array<char, 4096> blocks{};
-
-    ChunkPosition position;
-
-    std::unique_ptr<Mesh> mesh = nullptr;
-
 public:
-    std::atomic<ChunkState> state{ChunkState::AwaitingData};
-
-    std::atomic<int> awaitingData{7};
-
-    std::shared_ptr<Chunk> neighbors[6] = {};
-
     inline static const int NEIGHBORS[6][3] = {
         {+0, +0, +1}, // front
         {+0, +0, -1}, // back
@@ -66,22 +83,7 @@ public:
         {+0, -1, +0}, // bottom
     };
 
-    std::size_t index{0};
+    static void buildMesh(ChunkKey key, ChunkPack chunkPack, MeshChunk &mesh);
 
-    Chunk(ChunkPosition position) : position(position) {}
-
-    void setBlock(int x, int y, int z, char type) { blocks[getIndex(x, y, z)] = type; }
-
-    char getBlock(int x, int y, int z) const { return blocks[getIndex(x, y, z)]; }
-
-    ChunkPosition getPosition() const { return position; }
-
-    Mesh *getMesh() const { return mesh.get(); }
-
-    void setMesh(std::unique_ptr<Mesh> mesh) { this->mesh = std::move(mesh); }
-
-    std::unique_ptr<ChunkUpload> buildMesh();
-
-private:
-    inline int getIndex(int x, int y, int z) const { return (x << 8) | (y << 4) | z; }
+    inline static int getIndex(int x, int y, int z) { return (x << 8) | (y << 4) | z; }
 };
--- engine/src/chunk.cpp
+++ engine/src/chunk.cpp
@@ -1,11 +1,7 @@
 #include <engine/chunk.hpp>
 
-#include <engine/world.hpp>
-
-std::unique_ptr<ChunkUpload> Chunk::buildMesh()
+void Chunk::buildMesh(ChunkKey key, ChunkPack chunkPack, MeshChunk &mesh)
 {
-    auto data = std::make_unique<ChunkUpload>(position);
-
     const float cubeFaces[6][4][8] = {
         {{0, 0, 1, +0, +0, +1, 0.0f, 0.0f}, {1, 0, 1, +0, +0, +1, 1.0f, 0.0f}, {1, 1, 1, +0, +0, +1, 1.0f, 1.0f}, {0, 1, 1, +0, +0, +1, 0.0f, 1.0f}},                                                                                                                                               // front
         {{1, 0, 0, +0, +0, -1, 0.0f, 0.0f}, {0, 0, 0, +0, +0, -1, 1.0f, 0.0f}, {0, 1, 0, +0, +0, -1, 1.0f, 1.0f}, {1, 1, 0, +0, +0, -1, 0.0f, 1.0f}}, {{1, 0, 1, +1, +0, +0, 0.0f, 0.0f}, {1, 0, 0, +1, +0, +0, 1.0f, 0.0f}, {1, 1, 0, +1, +0, +0, 1.0f, 1.0f}, {1, 1, 1, +1, +0, +0, 0.0f, 1.0f}}, // right
@@ -21,7 +17,8 @@ std::unique_ptr<ChunkUpload> Chunk::buildMesh()
         {
             for (int z = 0; z < 16; z++)
             {
-                char type = getBlock(x, y, z);
+                int index = Chunk::getIndex(x, y, z);
+                char type = chunkPack.main->blocks.at(index);
 
                 if (type == 0) continue;
 
@@ -37,17 +34,18 @@ std::unique_ptr<ChunkUpload> Chunk::buildMesh()
 
                     if (inBounds)
                     {
-                        if (getBlock(nx, ny, nz) == 0) visible = true;
+                        int index = Chunk::getIndex(nx, ny, nz);
+                        if (chunkPack.main->blocks.at(index) == 0) visible = true;
                     }
                     else
                     {
-                        std::shared_ptr<Chunk> neighbor = neighbors[face];
+                        auto neighbor = chunkPack.neighbors[face];
 
                         if (neighbor == nullptr)
                         {
                             visible = true;
                         }
-                        else if (neighbor->getBlock(nx & 15, ny & 15, nz & 15) == 0)
+                        else if (neighbor->blocks[Chunk::getIndex(nx & 15, ny & 15, nz & 15)] == 0)
                         {
                             visible = true;
                         }
@@ -57,24 +55,24 @@ std::unique_ptr<ChunkUpload> Chunk::buildMesh()
                     {
                         for (int vertex = 0; vertex < 4; vertex++)
                         {
-                            data->vertices.push_back(x + cubeFaces[face][vertex][0]);
-                            data->vertices.push_back(y + cubeFaces[face][vertex][1]);
-                            data->vertices.push_back(z + cubeFaces[face][vertex][2]);
+                            mesh.vertices.push_back(x + cubeFaces[face][vertex][0]);
+                            mesh.vertices.push_back(y + cubeFaces[face][vertex][1]);
+                            mesh.vertices.push_back(z + cubeFaces[face][vertex][2]);
 
-                            data->vertices.push_back(cubeFaces[face][vertex][3]);
-                            data->vertices.push_back(cubeFaces[face][vertex][4]);
-                            data->vertices.push_back(cubeFaces[face][vertex][5]);
+                            mesh.vertices.push_back(cubeFaces[face][vertex][3]);
+                            mesh.vertices.push_back(cubeFaces[face][vertex][4]);
+                            mesh.vertices.push_back(cubeFaces[face][vertex][5]);
 
-                            data->vertices.push_back(cubeFaces[face][vertex][6]);
-                            data->vertices.push_back(cubeFaces[face][vertex][7]);
+                            mesh.vertices.push_back(cubeFaces[face][vertex][6]);
+                            mesh.vertices.push_back(cubeFaces[face][vertex][7]);
                         }
 
-                        data->indices.push_back(indexOffset + 0);
-                        data->indices.push_back(indexOffset + 1);
-                        data->indices.push_back(indexOffset + 2);
-                        data->indices.push_back(indexOffset + 2);
-                        data->indices.push_back(indexOffset + 3);
-                        data->indices.push_back(indexOffset + 0);
+                        mesh.indices.push_back(indexOffset + 0);
+                        mesh.indices.push_back(indexOffset + 1);
+                        mesh.indices.push_back(indexOffset + 2);
+                        mesh.indices.push_back(indexOffset + 2);
+                        mesh.indices.push_back(indexOffset + 3);
+                        mesh.indices.push_back(indexOffset + 0);
 
                         indexOffset += 4;
                     }
@@ -82,6 +80,4 @@ std::unique_ptr<ChunkUpload> Chunk::buildMesh()
             }
         }
     }
-
-    return data;
 }
